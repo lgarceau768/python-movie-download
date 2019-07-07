c4886d8c58ca6885a4329f25aa5b6a93429261dc
unknown <luke.garceau@kavokerr.com>
2019-07-07 00:52:05 -0400

    Changed the overall purpose of the file to just pull down links from a given source

--- TODO.md
+++ TODO.md
@@ -1,22 +1,7 @@
-#CURRENT  07/05/2019                                                                                                               
-Started work on the file                                         
-- ~~got a basic understanding of pulling the links down and a basic use of Bs4 to get the links~~                                                                                
-
-# TODO:                                                                                                                   
-- ~~format list of links to a list of [name, link]~~                                                                       
-- find the link corresponding to a given movie                                                                         
-- download it to local location                                                                                        
-- move it to a file share                                                                                              
--__add try catches to EVERYTHING__                                                                                                                                                                                                            
-# CHANGES                                                                                                                 
-- ~~instead of inputing a movie - read a list of movies from a csv file~~                                                  
-- output progress of download                                                                                          
-- download one movie at a time                                                                                         
-- make the file listen for a file change                                                                               
-- check to see if the movie in the list is already downloaded                                                          
-- make it so this python file is always running       
-                                                                                                                          
-# EXPLORE                                                                                                                 
-- launching a python script with given args from google home                                                           
-- ~~__have an option for bulk import or just a single movie__~~                                                                
-- ~~setup logging? - probably not~~
+# Current 07/07/2019
+# Todo 
+1. Given movie single / bulk input
+2. Search putlocker for movie
+    - find the link with the closest word (most amount of same chars / or just look for the movie name in the link string)
+3. Return the link
+4. Output to text file
--- downloadMovie.py
+++ downloadMovie.py
@@ -1,14 +1,21 @@
-import os, sys, configparser, urllib.request, sys, re
+import os, sys, configparser, urllib.request, sys, re, requests, time
 from bs4 import BeautifulSoup
 
-# defined functions
+# read variables from config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+sourceFile = config.read('Settings', 'source file')
+outputFile = config.read('Settings', 'output file')
+
+#### defined functions
+# check to see if a file exists
 def checkExists(filename):
     cwd = os.getcwd()
-    fileLocation = cwd + filename
-    if not os.path.isfile(fileLocation):
+    #fileLocation = cwd + filename 
+    if not os.path.isfile(filename):
         with open(filename, 'w') as file:
             file.close()
-    
 
 # get the movie name
 def findMovieName(link):
@@ -23,77 +30,86 @@ def getLinks(url):
 
     # set up the user agent and opening the Url
     req = urllib.request.Request(url, headers={'User-Agent':'Morzilla/5.0'})
-    c = urllib.request.urlopen(req).read()
-
+    try:
+        c = urllib.request.urlopen(req).read()
+    except Exception as e:
+        print('Exception in reading url: '+url+' '+str(e))
+        return None
     # parsing the content of the url
     soup = BeautifulSoup(c, 'html.parser') # change to lxml for linux devices
 
     # interating through all links
     linkList = []
+    #print('url: '+url)
     for link in soup.find_all('a', href=True):
         # skip the line to go to the parent directory
-        if link['href'] == '../' or link['href'] =='Parent Directory': 
-            continue
+        #print(link['href'])
+        linkList.append(link['href'].replace('%20', ''))
         
-        else: # extract link if it is a file
-            if '.jpg' not in link['href'] and 'mkv' not in link['href']:
-                if '720' in link['href'] or '1080' in link['href']:
-                    linkList.append(link['href'].replace('%20', ''))
-            continue
-
-    # pull a given movie down
-
-    ## need to format the list into a list of f arrays [name,location]
-    for i in range(0, len(linkList)):
-        url = urllib.parse.urljoin(url, linkList[i])
-        name = linkList[i][:len(link)-4]
-        pattern = re.compile('\W')
-        name = re.sub(pattern, ' ', name)
-        linkList[i] = [name, url]
-
-    # remove duplicates
-    newList = []
-    added = set()
-    for val in linkList:
-        name = findMovieName(val[0])
-        if not name in added:
-            newList.append(val)
-            added.add(name)
-    linkList = newList
-
-    # write links to file
-    # checkExists('links.txt')
-    # with open('links.txt','w') as linkFile:
-    #     for link in linkList:
-    #         linkFile.write(str(link)+'\n')
+    
     return linkList
 
-# check if given move is in url
+# function to write file
+def writeFile(list, filename):
+    checkExists(filename)
+    with open(filename, 'w') as linkFile:
+        for line in list:
+            linkFile.write(line+'\n')
+        linkFile.close()
 
-# import the config file
-config = configparser.ConfigParser()
-config.read('config.ini')\
+# function to read the sources from the text file
+def readFile(file):
+    fileList = []
+    checkExists(searchFile)
+    with open(searchFile, 'r') as sourceFile:
+        read = sourceFile.readlines()
+        for line in read:
+            if len(line) > 2:
+                fileList.append(line.strip())
+    return fileList
 
-# pull the name of the movie from the command line
-# command line arguement bulk or single
-## format python downloadMovie.py <operation bulk/single> <filename/moviename>
-operation = str(sys.argv[1])
+# function to look for a given movie in a given source url
+def getMovieUrl(movieName):
+    movieNameSearch = movieName.replace(' ','+').lower()
+    links = getLinks(source+movieNameSearch)
+    for link in links:
+        movieNameSearch = movieNameSearch.replace('+','-')
+        if movieNameSearch in link:
+            return link
+                  
+
+####  main script
+# find the total time execution of the program
+startTime = time.time()
+
+# get operation and moviename
+operation = sys.argv[1].lower()
+name = sys.argv[2]
 movieName = ''
 fileName = ''
-if operation == 'bulk':
-    fileName = str(sys.argv[2])
-    cwd = os.getcwd()
-    if not os.path.isfile(cwd+fileName):
-        print('File name specified not valid: '+str(sys.argv[2]))
-        sys.exit(0)
-elif operation == 'single':
-    movieName = str(sys.argv[2])
+if operation == 'single':
+    movieName = name
+elif operation == 'bulk':
+    fileName = name
 else:
-    print('Invalid Operation either <bulk/single> not ' +str(operation))
+    print('Invalid operation <single/bulk> not: '+operation)
     sys.exit(0)
 
+# get search sources: - <single/bulk>
+sourceList = readFile(sourceFile)
+found = False
+movieList = []
+for source in sourceList:
+    if operation == 'single':
+        movieList.append(getMovieUrl(movieName))
+    if operation == 'bulk':
+        movies = readFile(fileName)
+        for movie in movies:
+            movieList.append(getMovieUrl(movie))
 
-# determine the year of the movie release
-#
-#
-#
+# now output the movies to a text file
+writeFile(movieList, outputFile)
+
+# find the total runtime of the program
+print(sys.argv[0][2:]+' took %s seconds' % (time.time() - startTime))
+            
